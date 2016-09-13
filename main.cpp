8a702afdf508df9e8f738316ed426f33fa40a145
Chen Zhao <Chen@Chens-Mac-mini.local>
2016-09-13 23:22:16 +0900

    temp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 #include <bson.h>
 #include <mongoc.h>
 #include <stdio.h>
-=======
-//
-//  main.cpp
-//  BTC2NoSQL
-//
-//
-//
->>>>>>> a0b0fb76dedfdef99193ee411aa1a2e64edaecbf
 
 
 //g++ -o main main.cpp -l curl $(pkg-config --cflags --libs libmongoc-1.0) -std=c++11
@@ -20,18 +12,21 @@ int main (int argc, char *argv[])
     mongoc_cursor_t *cursor;
     bson_error_t error;
     bson_oid_t oid;
-    bson_t *doc;
+    //bson_t *doc;
     
     mongoc_init ();
     
     client = mongoc_client_new ("mongodb://localhost:27017/");
     collection = mongoc_client_get_collection (client, "mydb", "mycoll");
     
+    /*
     doc = bson_new ();
     bson_oid_init (&oid, NULL);
     BSON_APPEND_OID (doc, "_id", &oid);
-    BSON_APPEND_UTF8 (doc, "hello", "world");
+    BSON_APPEND_UTF8 (doc, "aa?", "hoho!");
+    */
     
+    bson_new_from_json ((const uint8_t *)"{\"a\":1}", -1, &error);
     if (!mongoc_collection_insert (collection, MONGOC_INSERT_NONE, doc, NULL, &error)) {
         fprintf (stderr, "%s\n", error.message);
     }
